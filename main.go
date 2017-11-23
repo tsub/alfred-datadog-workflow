42f622f9a6e7559565fb4ccb9c40c7e06db537f1
Tsubasa Takayama <tsubasatakayama511@gmail.com>
2017-11-23 18:26:51 +0900

    Configure Datadog credentials

--- main.go
+++ main.go
@@ -1,11 +1,23 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/deanishe/awgo"
 )
 
 func run() {
 	wf := aw.New()
+
+	apiKey := flag.String("apikey", "", "Datadog api key")
+	appKey := flag.String("appkey", "", "Datadog application key")
+	flag.Parse()
+
+	if *apiKey == "" || *appKey == "" {
+		wf.NewWarningItem("Datadog api key or application key is not set!", "subtitle")
+		wf.SendFeedback()
+		return
+	}
 
 	wf.NewItem("First result!!").Subtitle("subtitle")
 
